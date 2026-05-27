e7e0af000b8373bb04d31651dead2de25d20291d
Shubh Tamrakar <166866724+Shubh-Tamrakar@users.noreply.github.com>
2026-05-28 01:06:30 +0530

    Update RegisterPage.jsx

--- Frontend/src/components/RegisterPage.jsx
+++ Frontend/src/components/RegisterPage.jsx
@@ -11,6 +11,7 @@ const RegisterPage = () => {
   const { register } = useAuth();
   const navigate = useNavigate();
 
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError('');
