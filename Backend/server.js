f75d1b5bb472d5b413186616f92c448748bf4a25
Shubh Tamrakar <shubhtamrakar9893@gmail.com>
2026-05-27 23:00:02 +0530

    fix: update CORS for production

--- Backend/server.js
+++ Backend/server.js
@@ -21,7 +21,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 // ─── Middleware ───────────────────────────────────────────────────
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'https://webcraftai-eight.vercel.app/',
+  origin:'https://webcraftai-eight.vercel.app/',
   credentials: true,   // cookies ke liye zaroori
 }));
 
